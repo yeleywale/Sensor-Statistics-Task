0922c4021d976a04ca76f6c10b027180ef469b3a
Akinwale Alex <waleander@gmail.com>
2023-01-21 21:56:58 +0100

    Implement Sensor

--- src/main/scala/org/assignment/sensors/entities/Sensor.scala
+++ src/main/scala/org/assignment/sensors/entities/Sensor.scala
@@ -1,31 +1,40 @@
 
 package org.assignment.sensors.entities
 
-import org.assignment.sensors.Sensor
-
 import scala.util.{Failure, Success, Try}
 
-case class Sensor( sensorId: String, humidity: Int) {
+case class Sensor( sensorId: String, humidity: Either[String, Int]) {
+
+  def toPrettyString: String =
+    s"[$sensorId] $humidity}"
 
 }
 
 object Sensor {
+
+
   def parse(row: Map[String, String]): Try[Sensor] =
     for {
-      sensorId <- parseString(row, "Sensor_Id")
-      humidity <-  parseInt(row, "Humidity")
+      sensorId <- parseString(row, "sensor-id")
+      humidity <- parseEither(row, "humidity")
+      //Write to CSV. Create CSv if not exist
     } yield {
       Sensor(sensorId, humidity)
+      // process Sensor Aggregate Data
     }
 
   private def parseInt(row: Map[String, String], key: String): Try[Int] = parseAs(row, key, _.toInt)
 
   private def parseString(row: Map[String, String], key: String): Try[String] = parseAs(row, key, x => x)
 
+  private def parseEither(row: Map[String, String], key: String): Try[Either[String, Int]] = 
+    parseAs(row, key, x => if(x == "NaN") Left(x) else Right(x.toInt))
+
+    
   private def parseAs[T](row: Map[String, String], key: String, parser: String => T): Try[T] =
     for {
       value <- getValue(row, key)
-
+ 
       t <- Try(parser(value))
 
     } yield t
