10c84426f1a656ac89fad59b4805afee4b23df5e
Akinwale Alex <waleander@gmail.com>
2023-01-21 21:59:04 +0100

    Implement Solution in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,13 @@
+import org.assignment.sensors.SensorParser
+
 object Main {
+
+  val ex = new SensorParser("/Users/akinwalealex/Scala-Personal/Sensor Statistics Task/src/main/resources")
+
+
+
+
+  val combinedMap = ex.minHumidity
   def main(args: Array[String]): Unit = {
     println("Hello world!")
   }
