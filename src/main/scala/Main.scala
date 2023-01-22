c8c6f226342b6233edace232244bab009ba03574
Akinwale Alex <waleander@gmail.com>
2023-01-22 14:03:03 +0100

    Set up file processing in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,44 @@
 import org.assignment.sensors.SensorParser
+import org.assignment.sensors.entities.SensorAggregateData
+
+import java.net.URLDecoder
+import scala.math.Ordering
 
 object Main {
 
-  val ex = new SensorParser("/Users/akinwalealex/Scala-Personal/Sensor Statistics Task/src/main/resources")
+  //pass the path to the resource here
+  val ex = new SensorParser(URLDecoder.decode(getClass.getResource("/").getPath, "UTF-8"))
 
+  private val groupedMap = (ex.avgHumidity.toList :: ex.minHumidity.toList :: ex.maxHumidity.toList :: Nil)
+    .flatMap(_.toList).groupBy(_._1).mapValues(_.map(_._2)).toList
 
-
-
-  val combinedMap = ex.minHumidity
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
+  private val AggregateData = groupedMap.map {
+    case (sensorId, data) => {
+      if (data(0) == 0) SensorAggregateData(sensorId, Left("NaN"), Left("NaN"), Left("NaN"))
+      else SensorAggregateData(sensorId, Right(data(0)), Right(data(1)), Right(data(2)))
+    }
   }
+
+//  def printAggregateData(sensorAggregateData: SensorAggregateData): String =
+
+
+  private val SortedAggregateData = AggregateData.sortWith { (x, y) =>
+    (x.averageHumidity, y.averageHumidity) match {
+      case (Right(x), Right(y)) => x > y
+      case (Right(_), Left(_)) => true
+      case (Left(_), Right(_)) => false
+      case (Left(x), Left(y)) => x < y
+    }
+  }
+  def main(args: Array[String]): Unit = {
+    val rs = SortedAggregateData.map(x => x.toPrettyString)
+    println("```")
+    println(s"Number of processed file: ${ex.count}")
+    println(s"number of processed measurement: ${ex.measurementSize} ")
+    println(s"Number of failed measurement: ${ex.allNaNSensor.size} ")
+    println("sensor-id,min,avg,max")
+    rs.foreach(println)
+    println("```")
+  }
+
 }
