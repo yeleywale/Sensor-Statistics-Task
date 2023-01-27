ec20d3cb9e76f45524230c6b7686d50572508f6c
Akinwale Alex <waleander@gmail.com>
2023-01-27 05:38:06 +0100

    Add dependency for scala test in build.sbt

--- build.sbt
+++ build.sbt
@@ -7,6 +7,6 @@ val ScalaCsvVersion = "1.3.10"
 lazy val root = (project in file("."))
   .settings(
     name := "Sensor Statistics Task",
-    libraryDependencies += "com.github.tototoshi" %% "scala-csv" % ScalaCsvVersion
+    libraryDependencies += "com.github.tototoshi" %% "scala-csv" % ScalaCsvVersion,
+    libraryDependencies += "org.scalatestplus" %% "scalacheck-1-17" % "3.2.15.0" % "test"
   )
-
---
+++ src/main/scala/org/assignment/sensors/fs2/SensorParserFs2.scala
@@ -0,0 +1,71 @@
+package org.assignment.sensors.fs2
+
+
+import cats.effect.{Blocker, Concurrent, ExitCode, IO, IOApp}
+import fs2.*
+import fs2.text.*
+import fs2.io.*
+import fs2.io.file.{Files, Path}
+
+import java.io.File
+import java.net.URLDecoder
+import scala.util.{Try, Using}
+
+object SensorStatistics extends IOApp {
+
+  val directory = URLDecoder.decode(getClass.getResource("/").getPath, "UTF-8")
+
+  case class SensorStat(sensorId: String, humidity: Either[String, Int])
+
+  def toInt(s: String): Option[Int] = {
+    try {
+      Some(s.toInt)
+    } catch {
+      case e: Exception => None
+    }
+  }
+  private def parseSensorStat(line: String): Option[SensorStat] = {
+    val splitLines = line.split(",")
+    Try(SensorStat(
+      sensorId = line.split(",")(0),
+      humidity = toInt(line.split(",")(1)) match
+        case Some(i) => Right(i)
+        case None => Left("NaN")
+    )).toOption
+  }
+
+  private def csvLineCount[F[_] : Files : Concurrent](path: Path): F[Long] =
+    Files[F].walk(path).filter(_.extName == ".csv").flatMap { p =>
+      Files[F].readAll(p).through(text.utf8.decode).through(text.lines).as(1L)
+        .csv()
+    }.compile.foldMonoid
+
+
+  def readSensorData(directory: Path): List[SensorStat] = {
+    Files[IO].list(directory)
+      .flatMap(file => file.extName("csv"))
+      .through(text.utf8.decode)
+      .through(text.lines)
+      .map(parseSensorStat)
+      .unNone
+  }
+
+
+
+  def csvParser[F[_]]: Pipe[F, Byte, List[String]] =
+    _.through(text.utf8Decode)
+      .through(text.lines)
+      .drop(1) // remove headers
+      .map(_.split(',').toList) // separate by comma
+
+  val parser: Stream[IO, Unit] =
+    csvLineCount(io.file.Path(directory)
+
+
+
+  override def run(args: List[String]): IO[ExitCode] = IO.blocking(
+
+
+  )
+
+}
