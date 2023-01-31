85280a8a612ee831fb3ad2d310402d4b4e6ba883
Akinwale Alex <waleander@gmail.com>
2023-01-31 06:08:06 +0100

    Change value and methods name in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,25 @@
-import org.assignment.sensors.SensorParser
-import org.assignment.sensors.entities.SensorAggregateData
+
+import com.akinwalex.sensorcsvprocessor.SensorParser
+import com.akinwalex.sensorcsvprocessor.entities.SensorAggregateData
 
 import java.net.URLDecoder
 import scala.math.Ordering
 
 object Main {
 
-  //pass the path to the resource here
-  val ex = new SensorParser(URLDecoder.decode(getClass.getResource("/").getPath, "UTF-8"))
+  val sensorParser = new SensorParser(args(0))
 
-  private val groupedMap = (ex.avgHumidity.toList :: ex.minHumidity.toList :: ex.maxHumidity.toList :: Nil)
+  val sensorGroupedById = (sensorParser.avgHumidity.toList :: sensorParser.minHumidity.toList :: sensorParser.maxHumidity.toList :: Nil)
     .flatMap(_.toList).groupBy(_._1).mapValues(_.map(_._2)).toList
 
-  private val AggregateData = groupedMap.map {
+  val aggregateSensorData = sensorGroupedById.map {
     case (sensorId, data) => {
-      if (data(0) == 0) SensorAggregateData(sensorId, Left("NaN"), Left("NaN"), Left("NaN"))
-      else SensorAggregateData(sensorId, Right(data(0)), Right(data(1)), Right(data(2)))
+      if (data.head == 0) SensorAggregateData(sensorId, Left("NaN"), Left("NaN"), Left("NaN"))
+      else SensorAggregateData(sensorId, Right(data.head), Right(data(1)), Right(data(2)))
     }
   }
 
-//  def printAggregateData(sensorAggregateData: SensorAggregateData): String =
-
-
-  private val SortedAggregateData = AggregateData.sortWith { (x, y) =>
+  val sortedAggregateSensorData = aggregateSensorData.sortWith { (x, y) =>
     (x.averageHumidity, y.averageHumidity) match {
       case (Right(x), Right(y)) => x > y
       case (Right(_), Left(_)) => true
@@ -30,12 +27,35 @@ object Main {
       case (Left(x), Left(y)) => x < y
     }
   }
+
   def main(args: Array[String]): Unit = {
-    val rs = SortedAggregateData.map(x => x.toPrettyString)
+
+    val sensorParser = new SensorParser(args(0))
+
+    val sensorGroupedById = (sensorParser.avgHumidity.toList :: sensorParser.minHumidity.toList :: sensorParser.maxHumidity.toList :: Nil)
+      .flatMap(_.toList).groupBy(_._1).mapValues(_.map(_._2)).toList
+
+    val aggregateSensorData = sensorGroupedById.map {
+      case (sensorId, data) => {
+        if (data.head == 0) SensorAggregateData(sensorId, Left("NaN"), Left("NaN"), Left("NaN"))
+        else SensorAggregateData(sensorId, Right(data.head), Right(data(1)), Right(data(2)))
+      }
+    }
+
+    val sortedAggregateSensorData = aggregateSensorData.sortWith { (x, y) =>
+      (x.averageHumidity, y.averageHumidity) match {
+        case (Right(x), Right(y)) => x > y
+        case (Right(_), Left(_)) => true
+        case (Left(_), Right(_)) => false
+        case (Left(x), Left(y)) => x < y
+      }
+    }
+
+    val rs = sortedAggregateSensorData.map(x => x.toPrettyString)
     println("```")
-    println(s"Number of processed file: ${ex.count}")
-    println(s"number of processed measurement: ${ex.measurementSize} ")
-    println(s"Number of failed measurement: ${ex.allNaNSensor.size} ")
+    println(s"Number of processed file: ${sensorParser.count}")
+    println(s"number of processed measurement: ${sensorParser.measurementSize} ")
+    println(s"Number of failed measurement: ${sensorParser.allNaNSensor.size} ")
     println("sensor-id,min,avg,max")
     rs.foreach(println)
     println("```")
