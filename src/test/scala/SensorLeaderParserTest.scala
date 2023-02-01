4f8bd3ee6e52500be9c46bb0a593f5b66a686b84
Akinwale Alex <waleander@gmail.com>
2023-02-01 10:06:27 +0100

    Create test file

--- src/test/scala/SensorLeaderParserTest.scala
+++ src/test/scala/SensorLeaderParserTest.scala
@@ -1,6 +1,7 @@
 package com.akinwalex.sensorcsvprocessor.test
 
 import com.akinwalex.sensorcsvprocessor.SensorParser
+import com.akinwalex.sensorcsvprocessor.entities.SensorReport
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.BeforeAndAfter
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
@@ -9,23 +10,30 @@ import java.net.URLDecoder
 
 
 class SensorLeaderParserTest extends AnyFlatSpec with BeforeAndAfter with ScalaCheckPropertyChecks {
-   var sensorParser: SensorParser = _
+   var sensorReport: SensorReport = _
    var pathToResourceDirectory: String  = _
    before {
-      pathToResourceDirectory = URLDecoder.decode(getClass.getResource("/").getPath, "UTF-8")
-      sensorParser = new SensorParser(pathToResourceDirectory)
+      pathToResourceDirectory = "/Users/akinwalealex/Scala-Personal/Sensor Statistics Task/src/main/resources/"
+      sensorReport = new SensorReport(path = pathToResourceDirectory)
    }
 
+//   It reports how many files it processed
    "noOfFiles Sensor Parser object" should "return 4" in {
-       assert(sensorParser.noOfFiles == 4)
+       assert(sensorReport.csvFiles.size == 4)
    }
 
+//   It reports how many files it processed
    "The MeasurementSize(No of lines processed) excluding headers" should "be 429" in {
-      assert(sensorParser.measurementSize == 429)
+      assert(sensorReport.sensorReadings.size == 431)
    }
 
-
+//   It reports how many files it processed
    "get a list of sensors from a single csv file" should "list of sensors" in {
-      assert(sensorParser.getSensorsFromCSV("MOCK_DATA-3.csv").size == 104)
+      assert(sensorReport.failedSensors.size == 4)
+   }
+
+   //Sensors with only `NaN` measurements have min/avg/max as `NaN/NaN/NaN`
+   "get a list of sensors from a single csv file" should "list of sensors" in {
+      assert(sensorReport.aggregateSensorData.collect(x => x.maxHumidity).contains(Left()))
    }
 }
