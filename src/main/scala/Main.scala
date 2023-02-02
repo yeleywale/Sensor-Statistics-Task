36888bf22b133f4c1f3288c5628bef1271471c35
Akinwale Alex <waleander@gmail.com>
2023-02-02 04:10:34 +0100

    Reimplement extracted methods in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,13 +5,16 @@ import com.akinwalex.sensorcsvprocessor.entities.SensorReport
 object Main {
   def main(args: Array[String]): Unit = {
 
-    val sensorCalculation = SensorReport(args(0))
+    val sensorReport = SensorReport(args(0))
+
+    println(sensorReport.aggregateSensorData)
     println("```")
-    println(s"Number of processed file: ${sensorCalculation.csvFiles.size}")
-    println(s"number of processed measurement: ${sensorCalculation.sensorReadings.size} ")
-    println(s"Number of failed measurement: ${sensorCalculation.failedSensors.size} ")
+    println(s"Number of processed file: ${sensorReport.csvFiles.size}")
+    println(s"number of processed measurement: ${sensorReport.sensorReadings.size} ")
+    println(s"Number of failed measurement: ${sensorReport.failedSensors.size} ")
     println("sensor-id,min,avg,max")
-    sensorCalculation.report.foreach(println)
+    sensorReport.report.foreach(println)
     println("```")
   }
 }
+  
